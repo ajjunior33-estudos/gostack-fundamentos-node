2227ca4498aef5a434963d0d5dfe654b395f9e1e
André Souza <ajjunior33@gmail.com>
2020-10-31 21:53:24 -0200

    Finish.

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
     // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    return response.status(200).json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -18,6 +21,15 @@ transactionRouter.get('/', (request, response) => {
 transactionRouter.post('/', (request, response) => {
   try {
     // TODO
+    const { title, type, value } = request.body;
+    console.log(title, type, value);
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transation = createTransaction.execute({ title, value, type });
+
+    return response.status(200).json(transation);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,15 +1,37 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
-
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
   }
-
-  public execute(): Transaction {
+  public execute({ title, value, type }: Request): Transaction {
     // TODO
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You not have enough balance.');
+    }
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid.');
+    }
+
+    console.log(total);
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
